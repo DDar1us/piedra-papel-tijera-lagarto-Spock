680230e5e9c3a38040f4ddea703ffec78980fbc7
DDar1us <darius05g4@hotmail.com>
2013-09-30 11:28:54 -0300

    error arreglado,creo

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -5,35 +5,39 @@ package edu.tallerweb.pptls;
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-	private Mano jugadoruno
-	public void Mano(Forma forma) {
-	}
+	private Forma forma;
+
+
+
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será
 	 * la que determine su condición en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
-	
-	public Mano (Forma forma) {
-		return this.jugadoruno(forma);
+	public Mano(final Forma forma) {
+		this.forma=forma;
 	}
-	
+
+
+
 	/**
 	 * Evaluará el resultado de la partida según las reglas
 	 * del juego.
 	 * @param otra, la otra Mano.
+	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
-	public Resultado jugarCon(final Mano otra){
-		if (jugadoruno==otra){
+	public Resultado jugarCon(final Mano otra) {
+		if (this.forma==otra.forma) {
 			return Resultado.EMPATA;
 		}
-		if (this.jugadoruno==Forma.PIEDRA&&jugadordos==TIJERA||jugadoruno==PAPEL&&jugadordos==PIEDRA||jugadoruno==TIJERA&&jugadordos==PAPEL||jugadoruno==LAGARTO&&jugadordos==SPOCK||jugadoruno==SPOCK&&jugadordos==TIJERA||jugadoruno==TIJERA&&jugadordos==LAGARTO||jugadoruno==LAGARTO&&jugadordos==PAPEL||jugadoruno==PAPEL&&jugadordos==SPOCK||jugadoruno==SPOCK&&jugadordos==PIEDRA||jugadoruno==PIEDRA&&jugadordos==LAGARTO)
+		if (this.forma==Forma.PIEDRA&&otra.forma==Forma.TIJERA||this.forma==Forma.PAPEL&&otra.forma==Forma.PIEDRA||this.forma==Forma.TIJERA&&otra.forma==Forma.PAPEL||this.forma==Forma.LAGARTO&&otra.forma==Forma.SPOCK||this.forma==Forma.SPOCK&&otra.forma==Forma.TIJERA||this.forma==Forma.TIJERA&&otra.forma==Forma.LAGARTO||this.forma==Forma.LAGARTO&&otra.forma==Forma.PAPEL||this.forma==Forma.PAPEL&&otra.forma==Forma.SPOCK||this.forma==Forma.SPOCK&&otra.forma==Forma.PIEDRA||this.forma==Forma.PIEDRA&&otra.forma==Forma.LAGARTO)
 		{
+			return Resultado.GANA;
+		}
 		else
 		{
 			return Resultado.PIERDE;
 		}
-		}
+	
 	}
-
 }
